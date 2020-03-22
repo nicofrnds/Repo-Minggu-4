f0120489c032f95a0db6893cd3353f7fc26a18b4
nicofrnds <62508863+nicofrnds@users.noreply.github.com>
2020-03-23 00:49:25 +0700

    Upload File Tugas

---
+++ Praktikum_3_5_2/inputlogin.php
@@ -0,0 +1,17 @@
+<html>
+
+<head>
+    <title>Login Here</title>
+</head>
+
+<body>
+    <FORM action=" proseslogin.php" method="POST" name="input">
+        <h2>Login Ini</h2>
+        Username : <input type="text" name="username"><br>
+        Password : <input type="password" name="password"><br>
+        <input type="submit" name="Login" value="Login">
+        <input type="reset" name="reset" value="Reset">
+    </FORM>
+</body>
+
+</html>
---
+++ Praktikum_3_5_5/inputcombobox.php
@@ -0,0 +1,23 @@
+<html>
+
+<head>
+    <title>Film Kartun Favorit ~ Inputan Combo
+        box</title>
+</head>
+
+<body>
+    <form action="prosescombobox.php" method="post" name="input">
+        <h2>Pilih Film Kartun Favorit Anda :</h2>
+        <select name="kartun">
+            <option value="Sponge Bob">Sponge Bob</option>
+            <option value="Sinchan">Sinchan</option>
+            <option value="Conan">Conan</option>
+            <option value="Doraemon">Doraemon</option>
+            <option value="Dragon Ball">Dragon Ball</option>
+            <option value="Naruto">Naruto</option>
+        </select>
+        <input type="submit" name="Pilih" value="Pilih">
+    </form>
+</body>
+
+</html>
